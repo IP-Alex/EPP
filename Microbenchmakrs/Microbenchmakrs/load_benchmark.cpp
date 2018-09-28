6fd1837f7fcc7188a1ecb59b5231ab0f33a96e64
Tim Svensson <tisv1227@user.uu.se>
2018-09-28 15:56:06 +0200

    added stuff to the file

--- Microbenchmakrs/Microbenchmakrs/load_benchmark.cpp
+++ Microbenchmakrs/Microbenchmakrs/load_benchmark.cpp
@@ -3,12 +3,12 @@
 #include <omp.h>
 #include <time.h>
 
-
-
 #define L1_BYTES (256e3)
 #define L2_BYTES (1e6)
 #define L3_BYTES (1e6)
 
+#define NUMBER_OF_RUNS
+#define ITERATIONS_PER_RUN
 
 
 #define ARRAY_SIZE
