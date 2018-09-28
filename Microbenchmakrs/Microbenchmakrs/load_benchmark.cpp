8b54bb5dc5c54f5a5492cba6f1fd128b84fc24d2
Tim Svensson <tisv1227@user.uu.se>
2018-09-28 15:53:02 +0200

    added file 'load_benchmark.cpp'

---
+++ Microbenchmakrs/Microbenchmakrs/load_benchmark.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+
+
+#define L1_BYTES (256e3)
+#define L2_BYTES (1e6)
+#define L3_BYTES (1e6)
+
+
+
+#define ARRAY_SIZE
+
+int main()
+{
+
+}
